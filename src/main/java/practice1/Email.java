6ac9406185126704089919d44074273303fc179d
helgamuller <melnikova.olga@gmail.com>
2025-04-25 12:46:13 -0400

    Initial commit

---
+++ src/main/java/practice1/Email.java
@@ -0,0 +1,7 @@
+package practice1;
+
+public class Email {
+    public void sendEmailConfirmation() {
+        System.out.println("Отправляем письмо клиенту...");
+    }
+}
---
+++ src/main/java/practice1/Invoice.java
@@ -0,0 +1,7 @@
+package practice1;
+
+public class Invoice {
+    public void generateInvoice() {
+        System.out.println("Генерируем счет...");
+    }
+}
